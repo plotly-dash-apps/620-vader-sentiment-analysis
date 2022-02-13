0471fd4c43aeecfe0c297c517096e02137f53a35
Austin Lasseter <austinlasseter@gmail.com>
2022-02-13 11:34:40 -0500

    vader import

--- app.py
+++ app.py
@@ -3,7 +3,8 @@ import dash_core_components as dcc
 import dash_html_components as html
 import plotly.graph_objs as go
 from dash.dependencies import Input, Output, State
-from collections import deque, Counter
+from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
+
 
 ########### Define your variables ######
 
