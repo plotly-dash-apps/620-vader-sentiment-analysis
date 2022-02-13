c8290af056f387123e21d22e6dfaa72e3de02742
Austin Lasseter <austinlasseter@gmail.com>
2022-02-13 11:11:34 -0500

    new app

--- app.py
+++ app.py
@@ -1,59 +1,58 @@
 import dash
-import dash_auth
 import dash_core_components as dcc
 import dash_html_components as html
-import plotly
+import plotly.graph_objs as go
+from dash.dependencies import Input, Output, State
+from collections import deque, Counter
 
+########### Define your variables ######
 
-# Keep this out of source code repository - save in a file or a database
-VALID_USERNAME_PASSWORD_PAIRS = {
-    'Mickey': 'Mouse', 'Donald': 'Duck'
-}
+myheading1='Try out a palindrome here!'
+initial_value='A nut for a jar of tuna'
+longtext='''
+        _Suggestions you might try:_
+        * A man, a plan, a canal: Panama!
+        * Go hang a salami I'm a lasanga hog
+        * God! Nate bit a Tibetan dog!
+        '''
+tabtitle = 'racecar'
+sourceurl = 'https://www.grammarly.com/blog/16-surprisingly-funny-palindromes/'
+githublink = 'https://github.com/austinlasseter/dash-simple-callback'
 
+########### Define a function for your callback:
+def my_function(letters):
+    return(letters[::-1])
+
+########### Initiate the app
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
-
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 server = app.server
-app.title='auth example'
-auth = dash_auth.BasicAuth(
-    app,
-    VALID_USERNAME_PASSWORD_PAIRS
+app.title=tabtitle
+
+########### Set up the layout
+
+app.layout = html.Div(children=[
+    html.H1(myheading1),
+    html.Div(children=[dcc.Markdown(longtext)]),
+    dcc.Input(id='my-id', value=initial_value, type='text'),
+    html.Div(id='my-div'),
+    html.Br(),
+    html.A('Code on Github', href=githublink),
+    html.Br(),
+    html.A("Data Source", href=sourceurl),
+    ]
 )
 
-app.layout = html.Div([
-    html.H1('Welcome to the app'),
-    html.H3('You are successfully authorized'),
-    dcc.Dropdown(
-        id='dropdown',
-        options=[{'label': i, 'value': i} for i in [1, 2, 3, 4, 5]],
-        value=1
-    ),
-    dcc.Graph(id='graph'),
-    html.A('Code on Github', href='https://github.com/austinlasseter/dash-auth-example'),
-    html.Br(),
-    html.A("Data Source", href='https://dash.plotly.com/authentication'),
-], className='container')
 
+########## Define Callback
 @app.callback(
-    dash.dependencies.Output('graph', 'figure'),
-    [dash.dependencies.Input('dropdown', 'value')])
-def update_graph(dropdown_value):
-    x_values = [-3,-2,-1,0,1,2,3]
-    y_values = [x**dropdown_value for x in x_values]
-
-    return {
-        'layout': {
-            'title': 'Graph of {}'.format(str(dropdown_value)),
-            'margin': {
-                'l': 20,
-                'b': 20,
-                'r': 10,
-                't': 60
-            }
-        },
-        'data': [{'x': x_values, 'y': y_values}]
-    }
+    Output(component_id='my-div', component_property='children'),
+    [Input(component_id='my-id', component_property='value')]
+)
+def update_output_div(input_value):
+    palin=my_function(input_value)
+    return f"You've entered '{input_value}', and your output is '{palin}'"
 
 ############ Deploy
 if __name__ == '__main__':
-    app.run_server(debug=True)
+    app.run_server()
